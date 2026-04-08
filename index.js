7c979c9178ae4fee0909c3ca8109d4b8c4181a8e
Harsha <153048828+hkunamne@users.noreply.github.com>
2026-04-08 12:34:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
- 
+
+app.get('/about', (req, res) => {
+  res.send('About us');
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
